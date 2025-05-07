9976ccdcb97ecc5837bf39afa280816c935f8493
sanaulparvej <perves15-6433@s.diu.edu.bd>
2025-05-08 00:31:14 +0600

    state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,89 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(TaskManagerApp());
+  runApp(CounterApp());
 }
-ghorardim
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+ CounterController counterController = CounterController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: GetBuilder(
+            init: counterController,
+            builder: (_){
+          return Text(
+            '${counterController.count}',
+            style: TextStyle(fontSize: 28),
+          );
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterController.incrementCount();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({super.key, required this.counterController});
+//
+//   final CounterController counterController;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Profile'),
+//       ),
+//       body: Center(
+//         child: Text(''),
+//       ),
+//     );
+//   }
+// }
+
+
+// class CounterController{
+//   RxInt count = 0.obs;
+//
+//   void incrementCount(){
+//     count++;
+//   }
+// }
+
+
+class CounterController extends GetxController{
+  int count = 0;
+
+  void incrementCount(){
+    count++;
+    update();
+  }
+}
